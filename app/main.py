b24121535135960d8da7c160994f0f4035c5a73d
Andrew Bloese <andrewbloese@gmail.com>
2022-05-08 20:09:09 -0700

    fixed?

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
+from copyreg import pickle
 from flask import Flask , abort , request, jsonify
-
 from util import summarize_text
+import numpy as np 
+import requests
+import pickle
+
+RESOURCE_URL = "https://github.com/brokegti/vectors/raw/697e925299c0155b6f827938ccc13582c9388624/vectors"
 
 
+vectors = None
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,7 +21,19 @@ def do_summary():
     reduceTo=float(request.json.get("reduceTo"))
     if text is None or reduceTo is None: 
         return ""    
-    return summarize_text( text , reduce_to=reduceTo)
+    return summarize_text( text , reduce_to=reduceTo, vectors=vectors)
+
+
+
 
 if __name__ == "__main__":
-    app.run()
+    if vectors is None: 
+        print("Downloading vector text . . . ")
+        response = requests.get(RESOURCE_URL)
+        vectors = pickle.loads(response.content)
+            
+
+        
+
+    print("App ready!")
+    app.run()   
--- app/util.py
+++ app/util.py
@@ -1,4 +1,5 @@
 #text rank based summary 
+import requests
 from cmath import sqrt
 import numpy as np
 import pandas as pd
@@ -12,10 +13,8 @@ stop_words = [
     'himself', 'the', 'among', "n't", 'and', 'herein', 'hers', 'hereby', 'therein', 'along', 'seems', 'during', 'done', 'sometimes', 'off', 'until', 'without', '‘ve', 'per', 'above', 'other', 'on', 'noone', 'down', 'whole', 'anyway', 'please', 'fifty', "'ll", 'do', 'whereafter', 'mine', 'have', 'meanwhile', 'no', 'name', 'amongst', 'my', 'besides', 'became', 'to', 'somewhere', 'her', 'thereafter', 'against', 'give', 'always', 'empty', 'becoming', 'eight', '’ll', 'due', 'get', 'cannot', 'around', 'seem', 'if', 'thus', 'former', 'nine', 'ca', 'me', 'even', 'take', 'really', 'could', 'before', 'using', 'others', 'yours', 'however', 'who', 'but', 'those', 'myself', '‘d', 'while', 'she', 'up', 'call', 'in', 'everyone', '‘ll', 'make', 'though', 'nowhere', 'whatever', 'side', 'hence', 'third', 'only', 'go', 'has', 'may', 'many', 'become', '‘m', 'an', 'either', 'most', 'seemed', 'namely', 'say', '’m', 'yourselves', 'not', 'much', 'would', 'out', 'whereas', 'wherein', 'we', 'afterwards', 'with', 'were', 'hereafter', 'how', 'something', 'formerly', 'it', 'very', 'hundred', 'someone', 'will', '’re', 'sixty', 'herself', 'whether', 'except', 'next', 'same', 'still', 'so', 'n’t', 'becomes', 'might', 'into', '‘re', 'anywhere', 'whence', 'more', 'within', "'re", 'because', 'quite', 'just', 'also', 'bottom', 'should', 'perhaps', 'yourself', 'thru', 're', 'otherwise', 'twelve', 'i', 'few', 'nothing', 'had', 'beyond', 'he', 'onto', 'else', 'first', 'part', 'never', 'us', 'full', 'thereby', 'serious', 'three', 'whoever', 'upon', 'him', 'a', 'your', 'doing', 'further', 'often', 'too', 'you', 'all', 'front', 'whose', 'when', '’d', 'ours', 'his', 'after', 'from', 'am', 'wherever', 'its', 'see', 'somehow', 'together', 'alone', 'almost', 'least', 'be', 'between', 'five', "'ve", 'already', 'regarding', 'four', 'via', 'last', 'is', 'behind', 'everything', 'itself', 'beforehand', 'nor', 'what', 'none', 'thence', 'did', 'they', 'anyone', 'under', 'twenty', '‘s', 'any', 'why', 'anything', 'move', 'show', "'m", 'that', 'anyhow', 'six', 'whom', 'are', 'these', 'seeming', 'whenever', 'where', 'yet', 'now', 'was', 'another', 'therefore', 'whereby', 'once', '’ve', 'neither', 'both', 'top', 'our', 'below', 'through', 'one', 'whereupon', 'throughout', 'such', '’s', 'been', 'own', 'again', 'some', 'unless', 'nevertheless', 'latterly', 'since', 'at', 'of', 'although', 'themselves', 'fifteen', 'every', 'there', 'rather', 'mostly', 'eleven', 'beside', 'various', 'than', 'ten', 'ourselves', 'put', 'them', 'towards', 'thereupon', "'s", 'as', 'back', 'whither', 'about', 'this', 'made', 'each', 'elsewhere', "'d", 'can', 'enough', 'across', 'here', 'which', 'n‘t', 'less', 'does', 'latter', 'moreover', 'indeed', 'sometime', 'amount', 'then', 'for', 'two', 'or', 'ever', 'everywhere', 'keep', 'over', 'forty', 'several', 'their', 'being', 'hereupon', 'toward', 'by', 'well', 'used', 'must', 'nobody'
 ]
 
-def summarize_text( text , reduce_to ): 
-    from zipfile import ZipFile
-    with ZipFile("glove.6B.100d.txt.zip") as ref: 
-        ref.extractall()
+def summarize_text( text , reduce_to, vectors ): 
+    
     #split into sentences
     sentences = sent_tokenize(text)
 
@@ -37,22 +36,9 @@ def summarize_text( text , reduce_to ):
 
     clean_sentences = [ remove_stopwords(r.split) for r in clean_sentences]
 
-
-    
-
-
     #create word vectors
     word_vectors = {}
     
-    
-
-    with open('glove.6b.100d.txt',encoding='utf-8') as f:
-        for line in f:
-            values = line.split()
-            word = values[0]
-            coefs = np.asarray(values[1:], dtype="float32")
-            word_vectors[word] = coefs
-
 
     sentence_vectors = [ ]
     for sent in clean_sentences: 
